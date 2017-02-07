788d492325d4effbe96f64cc58ffe77c243a440f
Calvin <calvin@calvinmbp.local>
2017-02-07 11:54:59 -0800

    removed unnecessary pics

--- js/engine.js
+++ js/engine.js
@@ -179,10 +179,7 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png',
-        'images/ready.png',
-        'images/win.png',
-        'images/lose.png'
+        'images/char-boy.png'
     ]);
     Resources.onReady(init);
 
